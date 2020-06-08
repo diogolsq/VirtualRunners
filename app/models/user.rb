43fb8cd09805eca4ed9b09019237c7522710df00
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-08 14:20:25 -0300

    creating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  validates :name, :username, :password, :level, :email, presence: true
+  validates :username, uniqueness: true
+  has_many :messages, dependent: :destroy
+  has_many :races, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ # has_one_attached :photo # Cloudinary
+
+ #  # we will let the geocode access our address and convert it to latitude and longitude. #mapbox
+ #  geocoded_by :address
+ #  after_validation :geocode, if: :will_save_change_to_address?
+
 end
