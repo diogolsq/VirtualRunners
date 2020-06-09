b2ea9e6fa57dd7ac0f68f5ac6d3b4ee6f83f0b3c
Nicole Ochoa <nochoa91@gmail.com>
2020-06-09 14:56:26 -0500

    Adding strava parameters to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 class User < ApplicationRecord
   validates :name, :username, :password, :level, :email, presence: true
   validates :username, uniqueness: true
@@ -10,13 +12,35 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:strava]
 
  # has_one_attached :photo # Cloudinary
 
  #  # we will let the geocode access our address and convert it to latitude and longitude. #mapbox
  #  geocoded_by :address
  #  after_validation :geocode, if: :will_save_change_to_address?
+
+  def self.find_for_strava_oauth(auth)
+    binding.pry
+    user_params = auth.slice("provider", "uid")
+    user_params.merge! auth.info.slice("email", "first_name", "last_name")
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+    user_params = user_params.to_h
+
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save
+    end
+
+    return user
+  end
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,8 +5,7 @@ require 'omniauth-strava'
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.omniauth :strava, ENV["STRAVA_CLIENT_ID"], ENV["STRAVA_CLIENT_SECRET"],
-    scope: "view_private,write",
-    info_fields: 'email, first_name, last_name'
+    scope: "activity:write,read"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
