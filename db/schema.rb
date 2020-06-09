ab0824f4ce0659b573e999e6715a5e2ae79fed9f
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-09 17:27:21 -0300

    solved geocode issue dont need to create a table to put the address anymore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_195509) do
+ActiveRecord::Schema.define(version: 2020_06_09_202435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,17 +34,6 @@ ActiveRecord::Schema.define(version: 2020_06_09_195509) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "addresses", force: :cascade do |t|
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "category"
-    t.bigint "track_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["track_id"], name: "index_addresses_on_track_id"
   end
 
   create_table "chats", force: :cascade do |t|
@@ -113,7 +102,6 @@ ActiveRecord::Schema.define(version: 2020_06_09_195509) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "addresses", "tracks"
   add_foreign_key "chats", "tracks"
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
