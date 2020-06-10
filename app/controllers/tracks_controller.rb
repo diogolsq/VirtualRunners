a0c245b7eedf850a372f5fb5bc98489879869ec4
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-10 19:09:45 -0300

    creating the logic to join and give up a specif track

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -27,6 +27,8 @@ class TracksController < ApplicationController
       user = User.find(race.user_id)
       @users << user
     end
+    @user = current_user
+    @racewithuser = Race.where(user_id:@user.id, track_id:@track.id)
 
     @markers = [{
       lat: @track.start_latitude,
