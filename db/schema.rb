1d68e04ecae230de959ae6f813bc40259ac067ac
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-10 15:43:41 -0300

    solving git issue

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_180022) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
