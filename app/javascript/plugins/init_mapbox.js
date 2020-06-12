711bc72dc2f3c1686d0ba62efdb984636713f927
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-12 13:40:06 -0300

    decrease the zoom to better see the path display in the track

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,7 +10,7 @@ import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 100, maxZoom: 15, duration: 0 });
 };
 
 
