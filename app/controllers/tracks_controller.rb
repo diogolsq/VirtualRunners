6f5d2e27fe3c3673f3e4f34cfc1f002e0fd08e0a
Nicole Ochoa <nochoa91@gmail.com>
2020-06-16 16:02:55 -0500

    finished track show version 1

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -55,33 +55,35 @@ class TracksController < ApplicationController
     @markers << @marker_end
 
     # retrieving individual api race results
-    if @race.status == "ongoing"
-      @race = @track.races.find_by(user: current_user)
-      client = Strava::Api::Client.new(
-          access_token: current_user.token
-      )
+    if @race.present?
+      if @race.status == "ongoing"
+        @race = @track.races.find_by(user: current_user)
+        client = Strava::Api::Client.new(
+            access_token: current_user.token
+        )
 
-      activities = client.athlete_activities
-      activity = activities.first
-      pactivity = activities[1]
+        activities = client.athlete_activities
+        activity = activities.first
+        pactivity = activities[1]
 
-      if pactivity
-        if pactivity.id.to_s == @race.strava_activity_id
-          @race.strava_activity_id = activity.id.to_s
-          @race.distance = activity.distance
-          @race.elapsed_time = activity.elapsed_time
-          @race.start_lat_lng = activity.start_latlng
-          @race.end_lat_lng = activity.end_latlng
-          @race.average_speed = activity.average_speed
-          @race.max_speed = activity.max_speed
-          if activity.distance >= pactivity.distance
-            @race.status = "finished"
-          else
-            @race.status = "ongoing"
+        if pactivity
+          if pactivity.id.to_s == @race.strava_activity_id
+            @race.strava_activity_id = activity.id.to_s
+            @race.distance = activity.distance
+            @race.elapsed_time = activity.elapsed_time
+            @race.start_lat_lng = activity.start_latlng
+            @race.end_lat_lng = activity.end_latlng
+            @race.average_speed = activity.average_speed
+            @race.max_speed = activity.max_speed
+            if activity.distance >= pactivity.distance
+              @race.status = "finished"
+            else
+              @race.status = "ongoing"
+            end
+            @race.save
           end
-          @race.save
         end
-      end
+    end
 
     end
     @ongoing_races = Race.where(status: "ongoing", track_id: @track.id)
